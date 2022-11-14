8d3cfc0963ea468f3b33b51172689c16cf28cbc1
satoru <satorulogic@gmail.com>
2022-11-14 17:23:38 +0800

    Add endpoint for loading configuration

--- app.go
+++ app.go
@@ -23,6 +23,14 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
+func (a *App) LoadConfigs() *configs.Config {
+	cfg, err := configs.Load()
+	if err != nil {
+		return nil
+	}
+	return cfg
+}
+
 func (a *App) SaveConfigs(email, token, baseURL string) {
 	cfg := configs.Config{
 		Email:   email,
