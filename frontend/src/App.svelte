a955694e57e425c45f500e11da6de78310509194
satoru <satorulogic@gmail.com>
2022-11-14 17:24:07 +0800

    Only show configs form when new configs need to be saved

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,24 +1,31 @@
 <script>
-  import {SaveConfigs} from '../wailsjs/go/main/App.js'
+  import { LoadConfigs } from '../wailsjs/go/main/App.js'
+  import { onMount } from 'svelte'
+  import SaveConfigs from "./SaveConfigs.svelte";
 
+  let configLoaded = false;
   let email, token, baseURL
 
   function saveConfigs() {
     SaveConfigs(email, token, baseURL)
   }
+
+  onMount(async () => {
+    LoadConfigs().then(result => {
+      if (result) {
+        configLoaded = true
+        email = result.Email
+        token = result.Token
+        baseURL = result.BaseURL
+      }
+    })
+  })
 </script>
 
 <main>
-  <form on:submit|preventDefault={saveConfigs}>
-    <label>
-      BaseURL: <input type="text" bind:value={baseURL} >
-    </label>
-    <label>
-      Email: <input type="text" bind:value={email}>
-    </label>
-    <label>
-      Token: <input type="password" bind:value={token}>
-    </label>
-    <button type="submit">Submit</button>
-  </form>
+    {#if !configLoaded}
+        <SaveConfigs/>
+    {:else}
+        <div>Your email is {email}</div>
+    {/if}
 </main>
---
+++ frontend/src/SaveConfigs.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { SaveConfigs } from '../wailsjs/go/main/App.js'
+    let email, token, baseURL
+
+    function saveConfigs() {
+        SaveConfigs(email, token, baseURL)
+    }
+</script>
+
+<form on:submit|preventDefault={saveConfigs}>
+    <label>
+        BaseURL: <input type="text" bind:value={baseURL} >
+    </label>
+    <label>
+        Email: <input type="text" bind:value={email}>
+    </label>
+    <label>
+        Token: <input type="password" bind:value={token}>
+    </label>
+    <button type="submit">Submit</button>
+</form>
