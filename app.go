f35cb0e874718d88565e4ac6bc38fc0f17b17e58
satoru <satorulogic@gmail.com>
2022-11-14 08:10:54 +0800

    Expose endpoint for saving configs

--- app.go
+++ app.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"github.com/suzaku/jissuer/pkgs/configs"
 )
 
 // App struct
@@ -24,4 +27,15 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) SaveConfigs(email, token, baseURL string) {
+	cfg := configs.Config{
+		Email:   email,
+		Token:   token,
+		BaseURL: baseURL,
+	}
+	if err := cfg.Save(); err != nil {
+		log.Fatal(err)
+	}
 }
